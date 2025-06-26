80e95b69158c213418b744d7bbd052d6527795f5
EcSolticia <ecsolticia@gmail.com>
2025-06-26 22:31:17 +0600

    basic string processing

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,16 @@
 fn main() {
     let markdown: String = String::from("# hello\nbye\n# okay?")  ;
 
-    println!("{}", markdown);
+    let lines= markdown.split('\n');
+    
+    // iterate over lines
+    for line in lines {
+        let first_char: char = line.chars().nth(0).unwrap();
+        if first_char == '#' {
+            println!("{}", line.to_uppercase());
+        } else {
+            println!("{}", line);
+        }
+    }
+
 }
